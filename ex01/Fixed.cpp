95c36189cbe2adaafb3e808e8a0a8b90f7d07d7e
Ann Melnikova <amelniko@paul-f4Br8s3.clusters.42paris.fr>
2025-02-28 11:52:28 +0100

    ex01 is done

--- ex01/Fixed.cpp
+++ ex01/Fixed.cpp
@@ -6,6 +6,18 @@ Fixed::Fixed(void) : num_value(0)
     return ;
 }
 
+Fixed::Fixed(const int num) : num_value(num << bits)
+{
+    std::cout << "Int constructor called" << std::endl;
+    return ;
+}
+
+Fixed::Fixed(const float num) : num_value(static_cast<int>(num * (1 << bits)))
+{
+    std::cout << "Float constructor called" << std::endl;
+    return ;
+}
+
 Fixed::Fixed(const Fixed &other)
 {
     std::cout << "Copy constructor called" << std::endl;
@@ -29,7 +41,6 @@ Fixed::~Fixed(void)
 
 int Fixed::getRawBits(void) const
 {
-    std::cout << "getRawBits member function called" << std::endl;
     return (this->num_value);
 }
 
@@ -37,4 +48,23 @@ void Fixed::setRawBits(int const raw)
 {
     this->num_value = raw;
     return ;
+}
+
+float Fixed::toFloat(void) const
+{
+    return (static_cast<float>(this->num_value) / (1 << bits));
+}
+
+int Fixed::toInt(void) const
+{
+    return (this->num_value >> bits);
+}
+
+std::ostream &operator<<(std::ostream& out, const Fixed& number)
+{
+    if (number.toFloat() == (int)number.toFloat())
+        out << std::fixed << std::setprecision(0) << number.toFloat();
+    else
+        out << std::fixed << std::setprecision(2) << number.toFloat();
+    return (out);
 }
--- ex01/Fixed.hpp
+++ ex01/Fixed.hpp
@@ -2,6 +2,7 @@
 # define FIXED_HPP
 
 # include <iostream>
+# include <iomanip>
 
 class Fixed
 {
@@ -11,12 +12,18 @@ class Fixed
 
     public:
         Fixed(void);
+        Fixed(const int num);
+        Fixed(const float num);
         Fixed(const Fixed &other);
         Fixed &operator=(const Fixed &other);
         ~Fixed(void);
 
         int getRawBits(void) const;
         void setRawBits(int const raw);
+        float toFloat(void) const;
+        int toInt(void) const;
 };
+
+std::ostream &operator<<(std::ostream& out, const Fixed& number);
 
 #endif
